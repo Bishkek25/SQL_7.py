d26df95026629d0d8a4d2fed800c334a0f720a1d
Bishkek25 <kunduz.qw8@gmail.com>
2024-10-24 11:38:06 -0700

    hm7.py

---
+++ classwork7.py2/hm7.py2.py
@@ -0,0 +1,17 @@
+with sq.connect('test.db') as connection:
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fio VARCHAR(100),
+    old INT DEFAULT 0
+    ) ''')
+    cursor.execute('''INSERT INTO users(fio,old)
+     VALUES
+     ('Bekbolo',60),
+     ('beka',123)''')
+    #     CREATE
+    cursor.execute('''SELECT fio,old FROM users''')
+
+    for row in cursor:
+
+    print(row)
